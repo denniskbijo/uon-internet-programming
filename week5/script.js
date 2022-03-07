5758aff9d63faecdb03b735888d10c8865110c90
denniskbijo <denniskbijo7@gmail.com>
2022-03-07 17:20:45 +0000

    format

--- week5/script.js
+++ week5/script.js
@@ -1,40 +1,39 @@
-function registerEvents(){
-    var button = document.getElementById('circle');
-    document.addEventListener('keydown', myKeyDown);
-    button.style.opacity = '1';
+function registerEvents() {
+  var button = document.getElementById("circle");
+  document.addEventListener("keydown", myKeyDown);
+  button.style.opacity = "1";
 }
 
-function myKeyDown(event){
-    console.log(event.keyCode);
-    if(event.keyCode == 37){
-        moveButtonLeft();
-    }
-    if(event.keyCode == 39){
-        moveButtonRight();
-    }
-
+function myKeyDown(event) {
+  console.log(event.keyCode);
+  if (event.keyCode == 37) {
+    moveButtonLeft();
+  }
+  if (event.keyCode == 39) {
+    moveButtonRight();
+  }
 }
 
-function moveButtonLeft(){
-    var button = document.getElementById('circle');
-    var buttonOffsetLeft = button.offsetLeft;
-    button.style.left = buttonOffsetLeft - 10 + 'px';
+function moveButtonLeft() {
+  var button = document.getElementById("circle");
+  var buttonOffsetLeft = button.offsetLeft;
+  button.style.left = buttonOffsetLeft - 10 + "px";
 }
 
-function moveButtonRight(){
-    var button = document.getElementById('circle');
-    var buttonOffsetLeft = button.offsetLeft;
-    button.style.left = buttonOffsetLeft + 10 + 'px';
+function moveButtonRight() {
+  var button = document.getElementById("circle");
+  var buttonOffsetLeft = button.offsetLeft;
+  button.style.left = buttonOffsetLeft + 10 + "px";
 }
 
-function setTimer(){
-    setInterval(changeButtonStyle, 10);
+function setTimer() {
+  setInterval(changeButtonStyle, 10);
 }
 
-function changeButtonStyle(){
-    var button = document.getElementById('circle');
-    console.log(button.style.opacity);
-    button.style.opacity = parseFloat(button.style.opacity) - 0.01;
+function changeButtonStyle() {
+  var button = document.getElementById("circle");
+  console.log(button.style.opacity);
+  button.style.opacity = parseFloat(button.style.opacity) - 0.01;
 }
 
-document.addEventListener('DOMContentLoaded', registerEvents);
+document.addEventListener("DOMContentLoaded", registerEvents);
