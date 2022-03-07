79b3325ed1956995990bae6b7bc1ea9cc82605c5
denniskbijo <denniskbijo7@gmail.com>
2022-03-07 17:42:24 +0000

    format

--- week5/script.js
+++ week5/script.js
@@ -8,22 +8,24 @@ var timer;
 
 function myKeyDown(event) {
   console.log(event.keyCode);
+  //clear existing timers
   clearInterval(timer);
+
   if (event.keyCode == 37) {
     timer = setInterval(moveButtonLeft, 100);
   }
   if (event.keyCode == 39) {
     timer = setInterval(moveButtonRight, 100);
   }
-  
+
   //Up button
   if (event.keyCode == 38) {
     timer = setInterval(moveButtonUp, 100);
   }
-  
+
   if (event.keyCode == 40) {
     timer = setInterval(moveButtonDown, 100);
-}
+  }
 }
 
 function moveButtonDown() {
