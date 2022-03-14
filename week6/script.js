c937d9456a31b007a6dbadb82143e8d8051f0b08
denniskbijo <denniskbijo7@gmail.com>
2022-03-14 15:24:59 +0000

    week6 exercise1-BarChart complete

--- week6/script.js
+++ week6/script.js
@@ -1,65 +1,44 @@
 function registerEvents() {
-  var button = document.getElementById("circle");
-  document.addEventListener("keydown", myKeyDown);
-  button.style.opacity = "1";
-}
-//global variable
-var timer;
-
-function myKeyDown(event) {
-  console.log(event.keyCode);
-  //clear existing timers
-  clearInterval(timer);
-
-  if (event.keyCode == 37) {
-    timer = setInterval(moveButtonLeft, 100);
-  }
-  if (event.keyCode == 39) {
-    timer = setInterval(moveButtonRight, 100);
-  }
-
-  //Up button
-  if (event.keyCode == 38) {
-    timer = setInterval(moveButtonUp, 100);
-  }
-
-  if (event.keyCode == 40) {
-    timer = setInterval(moveButtonDown, 100);
-  }
-}
-
-function moveButtonDown() {
-  var button = document.getElementById("circle");
-  var offsetValue = button.offsetTop;
-  button.style.top = offsetValue + 10 + "px";
-}
-
-function moveButtonUp() {
-  var button = document.getElementById("circle");
-  var offsetValue = button.offsetTop;
-  button.style.top = offsetValue - 10 + "px";
-}
-
-function moveButtonLeft() {
-  var button = document.getElementById("circle");
-  var buttonOffsetLeft = button.offsetLeft;
-  button.style.left = buttonOffsetLeft - 10 + "px";
-}
-
-function moveButtonRight() {
-  var button = document.getElementById("circle");
-  var buttonOffsetLeft = button.offsetLeft;
-  button.style.left = buttonOffsetLeft + 10 + "px";
-}
-
-function setTimer() {
-  setInterval(changeButtonStyle, 10);
-}
-
-function changeButtonStyle() {
-  var button = document.getElementById("circle");
-  console.log(button.style.opacity);
-  button.style.opacity = parseFloat(button.style.opacity) - 0.01;
+  renderChart();
 }
 
 document.addEventListener("DOMContentLoaded", registerEvents);
+
+function renderChart() {
+  const ctx = document.getElementById('myChart').getContext('2d');
+  const myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+  myChart
+}
