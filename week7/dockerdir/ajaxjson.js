20e3de620736231066c750b5e17405d454edcf4d
denniskbijo <denniskbijo7@gmail.com>
2022-03-21 18:18:21 +0000

    week7 exercise1-AJAX SendRequestAndError

--- week7/dockerdir/ajaxjson.js
+++ week7/dockerdir/ajaxjson.js
@@ -1,9 +1,25 @@
 window.onload = makeAjaxRequest;
 
+let xhr;
 function makeAjaxRequest(){
+    //Create XHR Object
     if(window.XMLHttpRequest){
         xhr = new XMLHttpRequest();   
     }else if(window.ActiveXObject){
         xhr = new ActiveXObject("Microsoft.XMLHTTP");
     }
+
+    if(xhr){
+        //Send XHR Request
+        xhr.open("GET", "counties.json", true);
+        xhr.send();
+        xhr.onreadystatechange = showContents;
+    }else{
+        //Handle error
+        document.getElementById("updatemessage").innerHTML = "Couldn't perform the request";
+    }
+}
+
+function showContents(){
+ console.log("showContns");
 }
