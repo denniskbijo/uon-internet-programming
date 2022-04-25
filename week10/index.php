6af71e18cd5ee51015dc72684a452556e024b02d
denniskbijo <denniskbijo7@gmail.com>
2022-04-25 15:50:08 +0100

    Added form with input with action=form.php

--- week10/index.php
+++ week10/index.php
@@ -9,6 +9,12 @@
     echo 'Hello' . $_GET["name"];
     echo 'Your Age is'. $_GET["age"];
     ?>
+
+    <form action="form.php" method="GET">
+        <input type="text" name="input1"/>
+        <button type="submit" name="submit" value="Submit">Submit</button>
+        
+    </form>
 </body>
 
 </html>
