60a41b1fa88241ec6be2900492923aa9282e930f
denniskbijo <denniskbijo7@gmail.com>
2022-04-25 15:37:22 +0100

    Using PHP Intelephense formatter

--- week10/index.php
+++ week10/index.php
@@ -1,10 +1,13 @@
 <html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- <?php echo '<h1>Hello World</h1>'; 
- echo 'Hello' . $_GET["name"];
- ?> 
- </body>
+
+<head>
+    <title>PHP Test</title>
+</head>
+
+<body>
+    <?php echo '<h1>Hello World</h1>';
+    echo 'Hello' . $_GET["name"];
+    ?>
+</body>
+
 </html>
